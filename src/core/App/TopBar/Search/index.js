72dccac73de424196b425f0fe0e55104661713f0
Sandra Masternak <sandra.masternak1@gmail.com>
2026-01-30 23:46:40 +0100

    created index.js for Search component

---
+++ src/core/App/TopBar/Search/index.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { toPeople } from "../../../../common/routes";
+import { Input, SearchIcon, Label } from "./styled";
+import { useOnSearchInputChange } from "./useOnSearchInputChange";
+
+export const Search = () => {
+  const location = useLocation();
+
+  const queryInUrl = new URLSearchParams(location.search).get("query") || "";
+
+  const [inputValue, setInputValue] = useState(queryInUrl);
+
+  useEffect(() => {
+    setInputValue(queryInUrl);
+  }, [queryInUrl]);
+
+  const onSearchInputChange = useOnSearchInputChange(setInputValue);
+
+  const isSearchForPeople = location.pathname.startsWith(toPeople());
+
+  return (
+    <Label>
+      <SearchIcon />
+      <Input
+        onChange={({ target: { value } }) =>
+          onSearchInputChange({ newValue: value, isSearchForPeople })
+        }
+        value={inputValue}
+        placeholder={`Search for ${isSearchForPeople ? "people" : "movies"}...`}
+      />
+    </Label>
+  );
+};
