92a49d4a5be8896436c81943ed3597221fc03c12
Sandra Masternak <sandra.masternak1@gmail.com>
2026-01-30 14:44:34 +0100

    created index.js for People component

---
+++ src/common/bothPageTypes/People/index.js
@@ -0,0 +1,36 @@
+import { getImageUrl } from "../TMDb/getImageUrl";
+import { toPersonDetails } from "../../routes";
+import { TileListLink, TileList } from "../TileList";
+import { Tile } from "../Tile";
+import personPlaceholder from "../images/personPlaceholder.png";
+
+export const People = ({ people, showJob, showCharacter }) => {
+  return (
+    <TileList>
+      {people.map((person) => (
+        <TileListLink
+          key={`${person.id}-${person.job}-${person.character}`}
+          to={toPersonDetails(person.id)}
+        >
+          <Tile
+            title={person.name}
+            small
+            center
+            image={getImageUrl({
+              path: person.profile_path,
+              size: "medium",
+            })}
+            imagePlaceholder={personPlaceholder}
+            subtitle={
+              showJob
+                ? person.job
+                : showCharacter
+                  ? person.character
+                  : undefined
+            }
+          />
+        </TileListLink>
+      ))}
+    </TileList>
+  );
+};
