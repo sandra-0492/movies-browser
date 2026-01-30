3d05964f7685108a209c71445114520529643bf0
Sandra Masternak <sandra.masternak1@gmail.com>
2026-01-30 14:34:59 +0100

    created routes

---
+++ src/common/routes.js
@@ -0,0 +1,4 @@
+export const toMovies = () => "/movies";
+export const toMovieDetails = (id) => `/movies/${id}`;
+export const toPeople = () => "/people";
+export const toPersonDetails = (id) => `/people/${id}`;
