cbe79f72a51fe61b6b06f79e707713087ea460c8
Sandra Masternak <sandra.masternak1@gmail.com>
2026-01-30 23:45:14 +0100

    created theme

---
+++ src/core/theme.js
@@ -0,0 +1,21 @@
+export const theme = {
+  colors: {
+    sceniceBlue: "#0044CC",
+    patternsBlue: "#D6E4FF",
+    black: "#000000",
+    codGray: "#181818",
+    stormGray: "#74788B",
+    waterloo: "#7E839A",
+    silver: "#C4C4C4",
+    mystic: "#E4E6F0",
+    whisper: "#F5F5F5",
+    white: "#FFFFFF",
+  },
+  breakpoints: {
+    mobileVerticalMax: 575,
+    mobileMax: 767,
+    tabletVerticalMax: 991,
+    tabletHorizontalMax: 1199,
+    laptopMax: 1392,
+  },
+};
