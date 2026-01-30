39b9ab6059b8c73aa6d1da1c7ca11710ed429e28
Sandra Masternak <sandra.masternak1@gmail.com>
2026-01-30 14:40:34 +0100

    created index.js & styled.js for Error component

---
+++ src/common/bothPageTypes/Page/Content/Error/index.js
@@ -0,0 +1,14 @@
+import { StyledLink, Header, Icon, Paragraph, Wrapper } from "./styled";
+
+export const Error = () => (
+  <Wrapper>
+    <Icon />
+    <Header>Ooops! Something went wrong</Header>
+    <Paragraph>
+      Please check your network connection
+      <br />
+      and try again
+    </Paragraph>
+    <StyledLink to="/">Back to home page</StyledLink>
+  </Wrapper>
+);
---
+++ src/common/bothPageTypes/Page/Content/Error/styled.js
@@ -0,0 +1,78 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { ReactComponent as Warning } from "./warning.svg";
+
+export const Wrapper = styled.div`
+  margin-top: 200px;
+  display: grid;
+  grid-gap: 24px;
+  grid-template-columns: 1fr;
+  justify-items: center;
+  text-align: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    margin-top: 50px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
+    margin-top: 24x;
+    grid-gap: 16px;
+  }
+`;
+
+export const Icon = styled(Warning)`
+  margin-bottom: 16px;
+  height: auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
+    width: 70px;
+    margin-bottom: 8px;
+  }
+`;
+
+export const Header = styled.div`
+  margin: 0;
+  font-size: 36px;
+  font-weight: 600;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 32px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
+    font-size: 16px;
+  }
+`;
+
+export const Paragraph = styled.p`
+  margin: 0;
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 1.3;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
+    font-size: 14px;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  font-family: "Open Sans", sans-serif;
+  font-weight: bold;
+  font-size: 14px;
+  border-radius: 5px;
+  color: ${({ theme }) => theme.colors.white};
+  padding: 16px 24px;
+  background: ${({ theme }) => theme.colors.sceniceBlue};
+  transition: filter 0.3s;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
+    font-size: 12px;
+  }
+
+  &:hover {
+    filter: brightness(120%);
+  }
+`;
