dd52c1681f0d353a9174a1f3fef61a9042a0660b
Sandra Masternak <sandra.masternak1@gmail.com>
2026-02-07 23:40:01 +0100

    updated detailsSaga component

--- src/common/detailsPage/detailsSaga.js
+++ src/common/detailsPage/detailsSaga.js
@@ -1,36 +1,29 @@
 import { all, call, put, takeLatest, delay } from "redux-saga/effects";
 import { demoDelay } from "../bothPageTypes/demoDelay";
 
-function* fetchDetailsHandler(action, getDetails, getCredits, actions) {
-  const id = action.payload;
-  yield delay(demoDelay);
-
-  try {
-    const [details, credits] = yield all([
-      call(getDetails, { id }),
-      call(getCredits, { id }),
-    ]);
-
-    const { cast = [], crew = [] } = credits;
-
-    yield put(
-      actions.fetchSuccess({
-        ...details,
-        cast,
-        crew,
-      }),
-    );
-  } catch (error) {
-    yield put(actions.fetchError());
-  }
-}
-
 export function* detailsSaga({ getDetails, getCredits, actions }) {
-  yield takeLatest(
-    actions.fetch.type,
-    fetchDetailsHandler,
-    getDetails,
-    getCredits,
-    actions
-  );
+  function* fetchDetailsHandler({ payload: id }) {
+    yield delay(demoDelay);
+
+    try {
+      const [details, credits] = yield all([
+        call(getDetails, { id }),
+        call(getCredits, { id }),
+      ]);
+
+      const { cast = [], crew = [] } = credits;
+
+      yield put(
+        actions.fetchSuccess({
+          ...details,
+          cast,
+          crew,
+        })
+      );
+    } catch (error) {
+      yield put(actions.fetchError());
+    }
+  }
+
+  yield takeLatest(actions.fetch.type, fetchDetailsHandler);
 }
