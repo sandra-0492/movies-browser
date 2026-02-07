8e6f421b0eb4924bc89cf79d4a77521fea1ab7fd
Sandra Masternak <sandra.masternak1@gmail.com>
2026-02-07 16:18:07 +0100

    updated and fixed typos in MovieDetails component

--- src/features/movieDetails/MovieDetails/index.js
+++ src/features/movieDetails/MovieDetails/index.js
@@ -12,6 +12,8 @@ import { getYearFromDate } from "../../../common/bothPageTypes/getYearFromDate";
 export function MovieDetails() {
   const movie = useSelector(selectors.selectDetails);
 
+  if (!movie) return null;
+
   const movieMetaData = {
     Production: movie.production_countries?.map(({ name }) => name).join(","),
     "Release date": movie.release_date,
@@ -24,11 +26,11 @@ export function MovieDetails() {
         <Tile
           image={getImageUrl({
             path: movie.poster_path,
-            sieze: "medium",
+            size: "medium",
           })}
           imagePlaceholder={moviePlaceholder}
           title={movie.title}
-          subitle={getYearFromDate(movie.release_date)}
+          subtitle={getYearFromDate(movie.release_date)}
           metaData={movieMetaData}
           hideMetaDataKeysOnMobile
           tags={movie.genres?.map(({ name }) => name)}
@@ -39,13 +41,14 @@ export function MovieDetails() {
           description={movie.overview}
         />
 
-        {!!movie.cast?.length && (
+        {movie.cast?.length > 0 && (
           <>
             <Header as="h2">Cast ({movie.cast.length})</Header>
             <People people={movie.cast} showCharacter />
           </>
         )}
-        {!!movie.crew?.length && (
+
+        {movie.crew?.length > 0 && (
           <>
             <Header as="h2">Crew ({movie.crew.length})</Header>
             <People people={movie.crew} showJob />
