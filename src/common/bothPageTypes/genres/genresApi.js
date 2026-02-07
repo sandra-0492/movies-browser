9412f451cadd98a7cfdcd3087746c2b017f2a7ef
Sandra Masternak <sandra.masternak1@gmail.com>
2026-02-07 16:08:52 +0100

    updated genresApi

--- src/common/bothPageTypes/genres/genresApi.js
+++ src/common/bothPageTypes/genres/genresApi.js
@@ -1,6 +1,8 @@
 import { fetchFromAPI } from "../TMDb/fetchFromAPI";
 
-export const getGenres = () =>
-  fetchFromAPI({
+export const getGenres = async () => {
+  const response = await fetchFromAPI({
     path: "/genre/movie/list",
   });
+  return response.genres;
+};
