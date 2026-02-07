8291a560aeb58e5f84cdc01341b02b11e9b83a72
Sandra Masternak <sandra.masternak1@gmail.com>
2026-02-07 16:16:22 +0100

    updated TileList component

--- src/common/bothPageTypes/TileList/index.js
+++ src/common/bothPageTypes/TileList/index.js
@@ -1,33 +1,34 @@
 import { Link } from "react-router-dom";
 import styled, { css } from "styled-components";
 
-export const tileList = styled.div`
+export const TileList = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(205px, 1fr));
-  grid-gap: 16px;
+  gap: 16px;
   margin-top: 32px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}x) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
     grid-template-columns: repeat(2, 1fr);
     margin-top: 12px;
   }
 
-  ${({ lessItems }) =>
-    lessItems &&
+  ${({ $lessItems }) =>
+    $lessItems &&
     css`
       grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
 
       @media (max-width: ${({ theme }) =>
-          theme.breakpoints.mobileVerticalMax}x) {
+          theme.breakpoints.mobileVerticalMax}px) {
         grid-template-columns: 1fr;
       }
     `}
 `;
 
-export const TileLitLink = styled(Link)`
+export const TileListLink = styled(Link)`
   display: grid;
   align-items: stretch;
   transition: transform 0.3s;
+  will-change: transform;
 
   &:hover {
     transform: scale(1.03);
