62d8c1671ad56f3e5a95719aef0f2c2ce92f9382
Sandra Masternak <sandra.masternak1@gmail.com>
2026-02-07 16:15:45 +0100

    updated Backdrop component

--- src/features/movieDetails/MovieDetails/Backdrop/index.js
+++ src/features/movieDetails/MovieDetails/Backdrop/index.js
@@ -3,34 +3,34 @@ import { Container } from "../../../../common/bothPageTypes/Container";
 import { Vote } from "../../../../common/bothPageTypes/Vote";
 import { getImageUrl } from "../../../../common/bothPageTypes/TMDb/getImageUrl";
 import { selectors } from "../../movieDetailsSlice";
-import { Iamge, Vignette, Title, Wrapper } from "./styled";
+import { Image, Vignette, Title, Wrapper } from "./styled";
 
 export const Backdrop = () => {
   const movie = useSelector(selectors.selectDetails);
 
+  if (!movie || !movie.backdrop_path) return null;
+
   return (
-    !!movie.backdrop_path && (
-      <Wrapper>
-        <Container>
-          <Iamge
-            url={getImageUrl({
-              path: movie.backdrop_path,
-              size: "big",
-            })}
-          >
-            <Vignette>
-              <Title>{movie.title}</Title>
-              <Vote
-                vote={{
-                  average: movie.vote_average,
-                  count: movie.vote_count,
-                }}
-                backdrop
-              />
-            </Vignette>
-          </Iamge>
-        </Container>
-      </Wrapper>
-    )
+    <Wrapper>
+      <Container>
+        <Image
+          url={getImageUrl({
+            path: movie.backdrop_path,
+            size: "big",
+          })}
+        >
+          <Vignette>
+            <Title>{movie.title || ""}</Title>
+            <Vote
+              vote={{
+                average: movie.vote_average || 0,
+                count: movie.vote_count || 0,
+              }}
+              backdrop
+            />
+          </Vignette>
+        </Image>
+      </Container>
+    </Wrapper>
   );
 };
--- src/features/movieDetails/MovieDetails/Backdrop/styled.js
+++ src/features/movieDetails/MovieDetails/Backdrop/styled.js
@@ -7,12 +7,15 @@ export const Wrapper = styled.div`
 export const Image = styled.div`
   background-image: url("${({ url }) => url}");
   height: 770px;
+  min-height: 400px;
+  max-height: 80vh;
   background-size: cover;
   background-position: center;
   margin-bottom: 64px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.laptopMax}px) {
     height: 55vw;
+    min-height: 300px;
     margin-bottom: 56px;
   }
 
@@ -28,20 +31,8 @@ export const Vignette = styled.div`
   justify-content: flex-end;
   flex-wrap: wrap;
   background:
-    linear-gradient(
-      0deg,
-      ${({ theme }) => theme.colors.black},
-      transparent 11%,
-      transparent 89%,
-      ${({ theme }) => theme.colors.black}
-    ),
-    linear-gradient(
-      90deg,
-      ${({ theme }) => theme.colors.black},
-      transparent 20%,
-      transparent 80%,
-      ${({ theme }) => theme.colors.black}
-    );
+    linear-gradient(to bottom, ${({ theme }) => theme.colors.black} 0%, transparent 50%, ${({ theme }) => theme.colors.black} 100%),
+    linear-gradient(to right, ${({ theme }) => theme.colors.black} 0%, transparent 50%, ${({ theme }) => theme.colors.black} 100%);
 
   @media (max-width: ${({ theme }) => theme.breakpoints.laptopMax}px) {
     padding: 8px;
@@ -52,6 +43,8 @@ export const Title = styled.div`
   font-size: 64px;
   color: ${({ theme }) => theme.colors.white};
   font-weight: 600;
+  line-height: 1.1;
+  word-break: break-word;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorizontalMax}px) {
     font-size: 48px;
