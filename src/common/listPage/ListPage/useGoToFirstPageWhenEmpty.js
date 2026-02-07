cb313e8385746940d76e953ab2d8cb9546516c1d
Sandra Masternak <sandra.masternak1@gmail.com>
2026-02-07 16:20:44 +0100

    updated useGoToFirstPageWhenEmpty

--- src/common/listPage/ListPage/useGoToFirstPageWhenEmpty.js
+++ src/common/listPage/ListPage/useGoToFirstPageWhenEmpty.js
@@ -1,12 +1,19 @@
+import { useEffect } from "react";
 import { useGoToPage } from "./useGoToPage";
 
-const { useEffect } = require("react");
-
-export const useGoToFirstPageWhenResultsEmpty = ({ status, results, page }) => {
+export const useGoToFirstPageWhenResultsEmpty = ({
+  status,
+  results,
+  page,
+}) => {
   const goToPage = useGoToPage();
 
   useEffect(() => {
-    if (status === "success" && results.length === 0 && page !== 1) {
+    if (
+      status === "success" &&
+      results?.length === 0 &&
+      page !== 1
+    ) {
       goToPage(1);
     }
   }, [status, results, page, goToPage]);
