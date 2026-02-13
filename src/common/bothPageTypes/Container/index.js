23b41372962531af0d8299d7ca1b915aa3cf76a2
Sandra Masternak <sandra.masternak1@gmail.com>
2026-02-13 16:58:50 +0100

    updated Container component

--- src/common/bothPageTypes/Container/index.js
+++ src/common/bothPageTypes/Container/index.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 1368px;
-  max-width: calc(100% - 2 * 12px);
-  margin: auto;
+  max-width: 1368px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 16px;
 `;
+
